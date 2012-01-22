4b01a974fc3d7d5caa61c11e6f887e650679768d
Ken Alex Fassone <nexusventuri@gmail.com>
2012-01-23 00:06:59 +0100

    running around problem 31... will I be able to solve it?

--- 31/lib/problem31.rb
+++ 31/lib/problem31.rb
@@ -1,24 +1,37 @@
 $:.unshift File.expand_path("../../../common/lib/", __FILE__)
 
 class Problem31
+  attr_reader :partial_split
   def initialize()
     @currencies=[200, 100, 50, 20, 10, 5, 2, 1]
     @partial_split = {}
   end
 
   #never consider 11111
-  def split(number)
+  def split(number, limit)
+    puts "i'm here, #{number} #{limit}"
     return @partial_split[number] if !@partial_split[number].nil?
-    splitted = currencies.keep_if{|x| x < number}.inject({}){|acc, coin| acc[coin] = Quotient.new(number, num); acc}
-    count = count_perfect_divisions(splitted)
+    splitted = @currencies.keep_if{|x| x <= number && x < limit}.inject([]){|acc, coin| acc << Quotient.new(number, coin); acc}
+    puts splitted.count
+    puts count_perfect_divisions(splitted)
 
-  end
-
-  def count_perfect_divisions(splitted)
-    splitted.inject(0) do |acc, value| 
-      acc = acc + 1 if !value[1].has_rest?
+    result = splitted.inject(0) do |acc, quotient|
+      acc += (0..quotient.times).inject(0) do |acc, section|
+        sub_splitting = quotient.rest + section * quotient.divisor
+        puts "sub_splitting #{sub_splitting}"
+        result = split(sub_splitting, quotient.divisor)
+        @partial_split[sub_splitting] = result
+        acc += result
+        puts "acc: #{acc} result for #{sub_splitting}: #{result}"
+        acc
+      end
       acc
     end
+    result + count_perfect_divisions(splitted)
+  end
+
+  def count_perfect_divisions(quotients)
+    quotients.inject(0){|acc, x| acc += 1 if x.rest == 0; acc}
   end
 
   def count_permutations_from_base(splitted)
@@ -37,5 +50,9 @@ class Quotient
 
   def has_rest?
     @rest != 0
+  end
+
+  def to_s
+    "Quotient #{@rest + @times * @divisor}/#{@divisor} = #{@times} + #{@rest}"
   end
 end
--- 31/spec/problem31_spec.rb
+++ 31/spec/problem31_spec.rb
@@ -2,33 +2,34 @@ require 'problem31'
 require 'solution_printer'
 
 describe Problem31 do
-  xit "should find number of ways to split 5p" do
-    Problem31.new.split(5).should == 4
+  it "should find number of ways to split 5p" do
+    Problem31.new.split(5, 10000).should == 4
+    puts Problem31.new.split(5, 10000)
   end
 
   xit "should find number of ways to split 6p" do
     Problem31.new.split(6).should == 5
   end
 
-  it "should count 0 for empty maps" do 
+  xit "should count 0 for empty maps" do 
     splitted = {}
     Problem31.new.count_perfect_divisions(splitted).should == 0
   end
 
-  it "should count 1 for map containing a single perfect divisor" do
+  xit "should count 1 for map containing a single perfect divisor" do
     splitted = {}
     splitted[2] = Quotient.new(4,2)
     Problem31.new.count_perfect_divisions(splitted).should == 1
   end
 
-  it "should count each perfect divisor" do 
+  xit "should count each perfect divisor" do 
     splitted = {}
     splitted[2] = Quotient.new(10,2)
     splitted[5] = Quotient.new(10,5)
     Problem31.new.count_perfect_divisions(splitted).should == 2
   end
 
-  it "should not count element that are not divided perfectly" do
+  xit "should not count element that are not divided perfectly" do
     splitted = {}
     splitted[5] = Quotient.new(15,5)
     splitted[10] = Quotient.new(15, 10)
