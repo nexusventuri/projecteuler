f04a49c944c421b3ad4af87763ba48551b0c0ba0
nexusventuri <nexusventuri@gmail.com>
2012-02-05 17:46:03 +0100

    Solved 3 again

---
+++ 3/lib/problem3.rb
@@ -0,0 +1,5 @@
+$:.unshift File.expand_path("../../../common/lib/", __FILE__)
+
+class Problem3
+
+end
---
+++ 3/spec/problem3_spec.rb
@@ -0,0 +1,13 @@
+require 'problem3'
+require 'solution_printer'
+require 'integer_utils'
+
+describe Problem3 do
+  it "should find factors" do
+    puts 36.get_factors
+  end
+
+  it "Should solve the problem" do
+    puts 600851475143.get_factors
+  end
+end
